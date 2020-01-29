dfd8df1dca8a776cdbc8271d7d839153c941a548
Ruaraidh Jay-Chalmers <ruaraidh.jay@gmail.com>
2020-01-29 15:12:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -109,6 +109,8 @@ app.post('/push', checkAuth.checkToken, async function (req, res)  {
 
 });
 
-app.listen(3100, function () {
-  console.log('Example app listening on port 3000!')
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port)
 });
