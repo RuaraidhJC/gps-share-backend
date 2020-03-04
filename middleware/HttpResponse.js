5578d7730f713cd7dbecf152ae4692816906f174
RuaraidhJC <ruaraidh.jay@gmail.com>
2020-03-04 19:43:05 +0100

    ok

--- middleware/HttpResponse.js
+++ middleware/HttpResponse.js
@@ -8,40 +8,42 @@ module.exports.BadRequestResponse = require('./http/BadRequestReponse');
 module.exports.UnauthorizedResponse = require('./http/UnauthorizedResponse');
 module.exports.Middleware = HttpResponse;
 
-function HttpResponse(thrownResponse, req, res, next) {
+async function HttpResponse(thrownResponse, req, res, next) {
     if (! thrownResponse instanceof OkResponse) {
         console.error(thrownResponse);
     }
     if (thrownResponse.notification) {
-        (async () => {
-            const User = require('../models/index')['User'];
-            const {userList, message, body} = {...thrownResponse.notification};
-            let messages = [];
-            for (let email of userList) {
-                const {notificationToken} = await User.findOne({where:{email: email}});
-                try {
-                    Expo.isExpoPushToken(notificationToken);
-                    messages.push({
-                        to: notificationToken,
-                        title: message,
-                        _displayInForeground: true,
-                        sound: 'default',
-                        body: body,
-                    })
-                } catch (error) {
-
-                }
+        console.log("notification: ", thrownResponse.notification);
+        const User = require('../models/index')['User'];
+        const {userList, message, body} = {...thrownResponse.notification};
+        let messages = [];
+        console.log(message);
+        for (let email of userList) {
+            const { notificationToken } = await User.findOne({where:{email: email}});
+            try {
+                Expo.isExpoPushToken(notificationToken);
+                messages.push({
+                    to: notificationToken,
+                    title: message,
+                    _displayInForeground: true,
+                    sound: 'default',
+                    body: body,
+                })
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        console.log("messages: ", messages);
+        if (messages.length) {
+            let chunks = expo.chunkPushNotifications(messages);
+            for (let chunk of chunks) {
+                const ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+                console.log(ticketChunk);
 
             }
-            if (messages.length) {
-                let chunks = expo.chunkPushNotifications(messages);
-                for (let chunk of chunks) {
-                    await expo.sendPushNotificationsAsync(chunk);
-                }
-            }
-        })();
+        }
     }
-    delete thrownResponse.notification;
+    //delete thrownResponse.notification;
     res.status(thrownResponse.status || 500).send(thrownResponse);
     next();
 }
